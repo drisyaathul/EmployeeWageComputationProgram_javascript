62c9fecb593acbfd5c61fab0280a39c75f009020
drisyaathul <drisyak809@gmail.com>
2023-06-10 02:27:23 +0800

    Added: Employee Attendance

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Employee Wage Computation Program");
+//UC1: To Check Employee Attendance
+
+let attendance = Math.floor(Math.random()*2);
+if (attendance === 1) {
+    console.log("Employee is Present");
+}else {
+    console.log("Employee is Absent");
+}
